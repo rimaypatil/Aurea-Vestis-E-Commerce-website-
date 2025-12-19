8ddc62d5795cfb458fe9ecd50e34a408bfcc1057
rimaypatil <rimaypatil2612@gmail.com>
2025-12-19 15:50:48 +0530

    fixing cors issue

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(helmet());
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://aureavestis.netlify.app/"
+        "https://aureavestis.netlify.app"
     ],
     credentials: true
 }));
